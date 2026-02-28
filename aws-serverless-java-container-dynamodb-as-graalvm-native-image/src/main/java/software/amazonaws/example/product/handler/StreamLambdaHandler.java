9315807bb01b14294053107f385787456f79de79
Vadym Kazulkin <v.kazulkin@gmail.com>
2026-02-28 10:10:57 +0100

    renamed projects

--- aws-serverless-java-container-dynamodb-as-graalvm-native-image/src/main/java/software/amazonaws/example/product/handler/StreamLambdaHandler.java
+++ aws-serverless-java-container-dynamodb-as-graalvm-native-image/src/main/java/software/amazonaws/example/product/handler/StreamLambdaHandler.java
@@ -11,10 +11,8 @@ import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
-import com.amazonaws.serverless.proxy.spring.SpringDelegatingLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-
 
 import software.amazonaws.Application;
 
@@ -23,18 +21,14 @@ public class StreamLambdaHandler implements RequestStreamHandler{
 	
 	private static final Logger logger = LoggerFactory.getLogger(StreamLambdaHandler.class);
 	
-    private static SpringDelegatingLambdaContainerHandler handler;
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
     static {
         try {
-            handler = new SpringDelegatingLambdaContainerHandler(Application.class);
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
         } catch (ContainerInitializationException e) {
             e.printStackTrace();
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
-    }
-    
-    public StreamLambdaHandler() {
-    	logger.info("init stream lambda handler");
     }
      
 
@@ -42,7 +36,7 @@ public class StreamLambdaHandler implements RequestStreamHandler{
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
     	logger.info("entered generic stream lambda handler");
-        handler.handleRequest(inputStream, outputStream, context);
+        handler.proxyStream(inputStream, outputStream, context);
         logger.info("finished generic stream lambda handler");
     }
     	
