bc47efeed23b59340d3ead5096762d2fad432c3a
Vadym Kazulkin <v.kazulkin@gmail.com>
2026-03-07 18:10:09 +0100

    removed unused controller

--- aws-spring-cloud-function-dynamodb/src/main/java/software/amazonaws/Application.java
+++ aws-spring-cloud-function-dynamodb/src/main/java/software/amazonaws/Application.java
@@ -2,9 +2,19 @@ package software.amazonaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class Application {
+	
+	@Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        // Custom configurations go here
+        return mapper;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
