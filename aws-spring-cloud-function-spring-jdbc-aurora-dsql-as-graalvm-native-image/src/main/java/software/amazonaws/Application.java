87eec15a80a1d400765195772efa68edd1a3b141
Vadym Kazulkin <v.kazulkin@gmail.com>
2026-03-14 10:18:39 +0100

    added example of spring cloud function with spring boot 4, java 25, spring jdbc, hikari and aurora dsql as graal vm native image

---
+++ aws-spring-cloud-function-spring-jdbc-aurora-dsql-as-graalvm-native-image/src/main/java/software/amazonaws/Application.java
@@ -0,0 +1,22 @@
+package software.amazonaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import tools.jackson.databind.ObjectMapper;
+
+@SpringBootApplication
+public class Application {
+	
+	@Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        // Custom configurations go here
+        return mapper;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ aws-spring-cloud-function-spring-jdbc-aurora-dsql-as-graalvm-native-image/src/main/java/software/amazonaws/example/product/dao/ProductDao.java
@@ -0,0 +1,60 @@
+package software.amazonaws.example.product.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import software.amazonaws.example.product.entity.Product;
+
+@Repository
+public class ProductDao {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	/**
+	 * create a product and return its id
+	 * 
+	 * @param product product
+	 * @return product id
+	 */
+	public int createProduct(Product product) throws Exception {
+		var productSequenceNextValQuery = "SELECT nextval('product_id')";
+		var id = this.jdbcTemplate.queryForObject(productSequenceNextValQuery, Integer.class);
+		var createProductQuery = "INSERT INTO products VALUES (?, ?, ?)";
+		this.jdbcTemplate.update(createProductQuery, id, product.name(), product.price());
+		return id;
+	}
+
+	/**
+	 * returns product by its id
+	 * 
+	 * @param id -product id
+	 * @return
+	 * @throws Exception
+	 */
+	public Optional<Product> getProductById(int id) throws Exception {
+		var findProductByIdQuery = "SELECT * FROM products WHERE id = ?";
+		try {
+			return this.jdbcTemplate.queryForObject(findProductByIdQuery, new ProductMapper(), id);
+		  } catch (EmptyResultDataAccessException e) {
+			return Optional.empty();
+		}
+	}
+
+	private static class ProductMapper implements RowMapper<Optional<Product>> {
+
+		@Override
+		public Optional<Product> mapRow(ResultSet rs, int rowNum) throws SQLException {
+			int id = rs.getInt("id");
+			String name = rs.getString("name");
+			int price = rs.getInt("price");
+			return Optional.of(new Product(id, name, price));
+		}
+	}
+}
